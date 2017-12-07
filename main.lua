e7023cc72af1d091d38696b635ae6426a7c404e3
Lucas Black <hmnotsocool@gmail.com>
2017-12-06 18:18:38 -0800

    self-collision, food, boundaries

--- main.lua
+++ main.lua
@@ -1,25 +1,49 @@
+math.randomseed(os.time())
 local snake = {}
 
-function love.load()
+function loadSnake()
 	for i = 1, 5 do
 		snake[#snake+1] = {x=(5-i), y=0, dir="right", prevDir = "right"}
 	end
 end
 
-local tilesize = 16
-local widthRange = (love.graphics.getWidth() / tilesize) - (love.graphics.getWidth() % tilesize)
-local heightRange = (love.graphics.getHeight() / tilesize) - (love.graphics.getHeight() % tilesize)
+function love.load()
+	loadSnake()
+end
+
+local tilesize = 20
+local widthRange = (love.graphics.getWidth() / tilesize) - 1
+local heightRange = (love.graphics.getHeight() / tilesize) - 1
+
+local function getRandomGridPoint()
+	return math.random(0, widthRange), math.random(0, heightRange)
+end
+
+local fx, fy = getRandomGridPoint()
 local food = {
-	x = math.random(0, widthRange),
-	y = math.random(0, heightRange),
+	x = fx,
+	y = fy,
 	width = tilesize,
 	height = tilesize
 }
 
-local timerMax = .5
+local timerMax = .08
 local timer = timerMax
 local dead = false
+
+function addSnakePiece(x, y, dir)
+	snake[#snake+1] = {
+		x = x,
+		y = y,
+		dir = dir,
+		prevDir = dir
+	}
+end
+
+local canMove = true
 function love.update(dt)
+	love.window.setTitle("snake | score: " .. #snake)
+	local firstBlock = snake[1]
 	if timer > 0 then
 		timer = timer - dt
 	else
@@ -36,6 +60,8 @@ function love.update(dt)
 			elseif block.dir == "down" then
 				block.y = block.y + 1
 			end
+			-- change every snake piece in the previous direction 
+			-- of the piece in front of it
 			if i >= 2 then
 				block.prevDir = block.dir
 				block.dir = snake[i-1].prevDir
@@ -43,27 +69,67 @@ function love.update(dt)
 		end
 		-- check if snake collided with itself
 		for i = 2, #snake do
-			local firstBlock = snake[1]
 			local block = snake[i]
 			if firstBlock.x == block.x and firstBlock.y == block.y then
 				dead = true
 			end
 		end
+		-- check if snake head collided with food
+		if firstBlock.x == food.x and firstBlock.y == food.y then
+			local lastBlock = snake[#snake]
+			if lastBlock.dir == "right" then
+				addSnakePiece(lastBlock.x - 1, lastBlock.y, "right")
+			elseif lastBlock.dir == "left" then
+				addSnakePiece(lastBlock.x + 1, lastBlock.y, "left")
+			elseif lastBlock.dir == "up" then
+				addSnakePiece(lastBlock.x, lastBlock.y + 1, "up")
+			elseif lastBlock.dir == "down" then
+				addSnakePiece(lastBlock.x, lastBlock.y - 1, "down")
+			end
+			food.x, food.y = getRandomGridPoint()
+			love.timer.sleep(.5)
+		end
+		canMove = true
 	end
-	if dead then
-		love.graphics.setBackgroundColor(255,0,0)
+	if dead or firstBlock.x < 0
+	or firstBlock.x > widthRange
+	or firstBlock.y < 0
+	or firstBlock.y > heightRange then
 		dead = false
+		snake = {}
+		love.timer.sleep(1.25)
+		loadSnake()
+		food.x, food.y = getRandomGridPoint()
+		timerMax = timerMax - .005
+		if timerMax < .03 then
+			timerMax = .03
+		end
 	end
 end
 
 function love.draw()
+	-- draw snake pieces with negative padding
 	for i = 1, #snake do
-		love.graphics.rectangle("fill", snake[i].x *tilesize+2, snake[i].y*tilesize+2, tilesize - 4, tilesize - 4)
+		if i == 1 then
+			love.graphics.setColor(35, 245, 100)
+		else
+			love.graphics.setColor(220,220,255)
+		end
+		love.graphics.rectangle("fill", snake[i].x *tilesize+3, snake[i].y*tilesize+3, tilesize - 6, tilesize - 6)
+	end
+	love.graphics.setColor(235, 250, 90)
+	love.graphics.rectangle("fill", food.x * tilesize + 3, food.y * tilesize + 3, tilesize - 6, tilesize - 6)
+
+	love.graphics.setColor(255, 255, 255, 10)
+	for y = 0, heightRange * tilesize, tilesize do
+		for x = 0, widthRange * tilesize, tilesize do
+			love.graphics.rectangle("line", x+1, y+1, tilesize-2, tilesize-2)
+		end
 	end
 end
 
 function love.keypressed(key)
-	if key == "right" or key == "left" or key == "up" or key == "down" then
+	if ( key == "right" or key == "left" or key == "up" or key == "down" ) and canMove then
 		if  not (snake[1].dir == "right" and key == "left" )
 		and not (snake[1].dir == "left"  and key == "right")
 		and not (snake[1].dir == "up"    and key == "down" )
@@ -71,5 +137,9 @@ function love.keypressed(key)
 			snake[1].dir = key
 		end
 		snake[1].prevDir = snake[1].dir
+		canMove = false
+	end
+	if key == "escape" then
+		love.event.quit()
 	end
 end
